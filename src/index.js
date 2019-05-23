97d66a36713315ae8d05db9e594d96349cbe4a2d
Maciej Korolik <maciej.korolik@gmail.com>
2019-05-23 12:45:46 +0200

    menu animations done for mobile

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-import "./style/style.css";
+import "./css/style.css";
+import "./index.html";
+import anime from "animejs/lib/anime.es.js";
+
+console.log("built");
+
+const hamburgerButton = document.querySelector(".hamburger");
+
+const menuSlide = anime({
+  targets: ".header__nav",
+  translateY: [-500, 0],
+  easing: "easeInOutQuad",
+  duration: 900,
+  autoplay: false
+});
+const menuShape = anime({
+  targets: ".path--nav",
+  d: {
+    value:
+      "M0,298s11,69,33,77,18-32,52-33,39,29,58,29,27-55,51-56,24,61,61,61,65-54,65-54V0H0Z"
+  },
+  duration: 900,
+  easing: "easeInOutQuad",
+  autoplay: false
+});
+menuSlide.reverse();
+menuShape.reverse();
+
+function toggleMenu() {
+  menuSlide.reverse();
+  menuShape.reverse();
+  menuSlide.completed = false;
+  menuShape.completed = false;
+  menuSlide.play();
+  menuShape.play();
+}
+
+hamburgerButton.addEventListener("click", () => {
+  toggleMenu();
+  hamburgerButton.classList.toggle("hamburger--active");
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,63 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
-                }
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        use: ["file-loader?name=[name].[ext]&outputPath=fonts/"]
+      },
+      {
+        test: /\.(html)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].html"
             }
-        },
-        {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader'],
-        },
-        {
-            test: /\.(png|svg|jpg|gif)$/,
-            use: [
-                    'file-loader?name=[name].[ext]&outputPath=img/'
-                ]
-        },
-        {
-            test: /\.(eot|ttf|woff|woff2)$/,
-            use: [
-                'file-loader?name=[name].[ext]&outputPath=fonts/'
-            ]
-        },
+          },
+          {
+            loader: "extract-loader"
+          },
+          {
+            loader: "html-loader",
+            options: {
+              attrs: ["img:src"]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "img/[name].[ext]"
+            }
+          }
+        ]
+      }
     ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
-    ]
+  }
 };
