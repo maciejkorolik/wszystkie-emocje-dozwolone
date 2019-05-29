c04a581fbfec70449155a976fabf010c69ad990b
Maciej Korolik <maciej.korolik@gmail.com>
2019-05-29 21:38:22 +0200

    header and home finished, about section started

--- src/index.js
+++ src/index.js
@@ -1,41 +1,45 @@
 import "./css/style.css";
 import "./index.html";
+import Rellax from "rellax";
 import anime from "animejs/lib/anime.es.js";
 
-console.log("built");
+const rellax = new Rellax(".rellax");
 
-const hamburgerButton = document.querySelector(".hamburger");
-
-const menuSlide = anime({
-  targets: ".header__nav",
-  translateY: [-500, 0],
-  easing: "easeInOutQuad",
-  duration: 900,
-  autoplay: false
-});
-const menuShape = anime({
-  targets: ".path--nav",
+const lineAnimation = anime({
+  targets: ".line-path",
   d: {
     value:
-      "M0,298s11,69,33,77,18-32,52-33,39,29,58,29,27-55,51-56,24,61,61,61,65-54,65-54V0H0Z"
+      "M.35,68.54c54.66-52.67,97.49,2.14,154,11,89.8,14.09,123.62-60.55,233-25,89.52,29.1,100.88,87.52,211,121,82.41,25.06,76.88-77,129-83,74.82-8.61,115.81,68.29,197,35,97.67-40.05,108.92-120.38,250-127,54.39-2.55,174.08,113.81,269,28"
   },
-  duration: 900,
-  easing: "easeInOutQuad",
+  easing: "easeInOutSine",
   autoplay: false
 });
-menuSlide.reverse();
-menuShape.reverse();
 
-function toggleMenu() {
-  menuSlide.reverse();
-  menuShape.reverse();
-  menuSlide.completed = false;
-  menuShape.completed = false;
-  menuSlide.play();
-  menuShape.play();
+const menuTriggers = [
+  document.querySelector(".hamburger"),
+  document.querySelector(".nav-list")
+];
+
+menuTriggers.forEach(el =>
+  el.addEventListener("click", () => {
+    document.querySelector(".hamburger").classList.toggle("hamburger--active");
+    document
+      .querySelector(".header__nav")
+      .classList.toggle("header__nav--active");
+    document
+      .querySelector(".menu-overlay")
+      .classList.toggle("menu-overlay--active");
+  })
+);
+
+function updatePath(e) {
+  const width = window.innerWidth;
+  let x = e.offsetLeft;
+
+  x = e.screenX;
+
+  const xPercentage = x / width;
+  lineAnimation.seek(lineAnimation.duration * xPercentage);
 }
 
-hamburgerButton.addEventListener("click", () => {
-  toggleMenu();
-  hamburgerButton.classList.toggle("hamburger--active");
-});
+window.addEventListener("mousemove", updatePath);
