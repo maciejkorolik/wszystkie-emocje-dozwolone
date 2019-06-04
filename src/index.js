3fcf6d7c22ec41ab3de54674caac644a2131c508
Maciej Korolik <maciej.korolik@gmail.com>
2019-06-04 12:49:40 +0200

    visual changes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import "./css/style.css";
+import "./css/shapes.css";
 import "./index.html";
 import Rellax from "rellax";
 import anime from "animejs/lib/anime.es.js";
 
 const rellax = new Rellax(".rellax");
 
-const lineAnimation = anime({
-  targets: ".line-path",
-  d: {
-    value:
-      "M.35,68.54c54.66-52.67,97.49,2.14,154,11,89.8,14.09,123.62-60.55,233-25,89.52,29.1,100.88,87.52,211,121,82.41,25.06,76.88-77,129-83,74.82-8.61,115.81,68.29,197,35,97.67-40.05,108.92-120.38,250-127,54.39-2.55,174.08,113.81,269,28"
-  },
-  easing: "easeInOutSine",
-  autoplay: false
-});
+// const lineAnimation = anime({
+//   targets: ".line-path",
+//   d: {
+//     value:
+//       "M.35,68.54c54.66-52.67,97.49,2.14,154,11,89.8,14.09,123.62-60.55,233-25,89.52,29.1,100.88,87.52,211,121,82.41,25.06,76.88-77,129-83,74.82-8.61,115.81,68.29,197,35,97.67-40.05,108.92-120.38,250-127,54.39-2.55,174.08,113.81,269,28"
+//   },
+//   easing: "easeInOutSine",
+//   autoplay: false
+// });
 
 const menuTriggers = [
   document.querySelector(".hamburger"),
@@ -32,14 +33,14 @@ menuTriggers.forEach(el =>
   })
 );
 
-function updatePath(e) {
-  const width = window.innerWidth;
-  let x = e.offsetLeft;
+// function updatePath(e) {
+//   const width = window.innerWidth;
+//   let x = e.offsetLeft;
 
-  x = e.screenX;
+//   x = e.screenX;
 
-  const xPercentage = x / width;
-  lineAnimation.seek(lineAnimation.duration * xPercentage);
-}
+//   const xPercentage = x / width;
+//   lineAnimation.seek(lineAnimation.duration * xPercentage);
+// }
 
-window.addEventListener("mousemove", updatePath);
+// window.addEventListener("mousemove", updatePath);
