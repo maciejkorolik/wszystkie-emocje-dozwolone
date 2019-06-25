f18fad5c801cb44118551b44b7237ad858565d21
maciejkorolik <maciej.korolik@gmail.com>
2019-06-25 16:47:02 +0200

    offer animations added

--- src/index.js
+++ src/index.js
@@ -6,20 +6,86 @@ import anime from "animejs/lib/anime.es.js";
 
 const rellax = new Rellax(".rellax");
 
-// const lineAnimation = anime({
-//   targets: ".line-path",
-//   d: {
-//     value:
-//       "M.35,68.54c54.66-52.67,97.49,2.14,154,11,89.8,14.09,123.62-60.55,233-25,89.52,29.1,100.88,87.52,211,121,82.41,25.06,76.88-77,129-83,74.82-8.61,115.81,68.29,197,35,97.67-40.05,108.92-120.38,250-127,54.39-2.55,174.08,113.81,269,28"
-//   },
-//   easing: "easeInOutSine",
-//   autoplay: false
-// });
+let offerVisible = false;
 
 const menuTriggers = [
   document.querySelector(".hamburger"),
   document.querySelector(".nav-list")
 ];
+const offerButtons = document.querySelectorAll(".btn--offer");
+
+function animateOfferButton(target, scale, duration, elasticity) {
+  anime.remove(target);
+  anime({
+    targets: target,
+    scale: scale,
+    duration: duration,
+    elasticity: elasticity
+  });
+}
+function enterOfferButton(target) {
+  !offerVisible && animateOfferButton(target, 1.1, 800, 400);
+}
+function leaveOfferButton(target) {
+  !offerVisible && animateOfferButton(target, 1.0, 600, 300);
+}
+
+function revealDetails(target) {
+  offerVisible = true;
+  const offerButton = target.querySelector(".btn--offer");
+  const currentOffer = target.dataset.offer;
+  const offerDetails = document.querySelector(
+    `.offer-details--offer${currentOffer}`
+  );
+  anime.remove(target);
+  anime.remove(offerButton);
+  document.body.style.overflow = "hidden";
+  anime({
+    targets: target,
+    scale: [
+      { value: 30, duration: 500 },
+      { value: 1, duration: 100, delay: 300 }
+    ],
+    zIndex: [
+      { value: 300, duration: 1 },
+      { value: 10, duration: 100, delay: 1000 }
+    ],
+    easing: "easeInOutSine",
+    loop: false
+  });
+  anime({
+    targets: offerButton,
+    opacity: [
+      { value: 0, duration: 1 },
+      { value: 1, duration: 1, delay: 1000 }
+    ],
+    loop: false
+  });
+  setTimeout(() => {
+    offerDetails.classList.add("offer-details--active");
+  }, 300);
+  offerDetails.addEventListener("click", event => {
+    event.currentTarget.classList.remove("offer-details--active");
+    offerVisible = false;
+    document.body.style.overflow = "visible";
+  });
+}
+
+offerButtons.forEach(el =>
+  el.addEventListener("mouseenter", event => {
+    enterOfferButton(event.currentTarget.parentNode), false;
+  })
+);
+offerButtons.forEach(el =>
+  el.addEventListener("mouseleave", event => {
+    leaveOfferButton(event.currentTarget.parentNode), false;
+  })
+);
+offerButtons.forEach(el =>
+  el.addEventListener("click", event => {
+    revealDetails(event.currentTarget.parentNode), false;
+  })
+);
 
 menuTriggers.forEach(el =>
   el.addEventListener("click", () => {
@@ -32,15 +98,3 @@ menuTriggers.forEach(el =>
       .classList.toggle("menu-overlay--active");
   })
 );
-
-// function updatePath(e) {
-//   const width = window.innerWidth;
-//   let x = e.offsetLeft;
-
-//   x = e.screenX;
-
-//   const xPercentage = x / width;
-//   lineAnimation.seek(lineAnimation.duration * xPercentage);
-// }
-
-// window.addEventListener("mousemove", updatePath);
